dafb3cdc6fecaad81ea1a9e0265c8d268528f92e
Olia Karelkina <ok.00100@gmail.com>
2019-12-23 23:41:11 -0500

    Update ArduinoFFTNeoPixel.ino

--- ArduinoFFTNeoPixel.ino
+++ ArduinoFFTNeoPixel.ino
@@ -1,6 +1,5 @@
 /*
 fft_adc_serial.pde
-guest openmusiclabs.com 7.7.14
 example sketch for testing the fft library.
 it takes in data on ADC0 (Analog0) and processes them
 with the fft. the data is sent out over the serial
